a4588bf5eae67605725e9314cde2ed15583c79bf
eyo-as <eyosiyastumiso7@gmail.com>
2025-03-09 18:12:06 +0300

    update create answer component

--- src/markup/components/answer/CreateAnswerComp.jsx
+++ src/markup/components/answer/CreateAnswerComp.jsx
@@ -1,13 +1,10 @@
-import { useEffect, useRef, useState } from "react";
-import ContentLeft from "../main-content/ContentLeft";
-import ContentRight from "../main-content/ContentRight";
+import { useRef, useState } from "react";
 import { useNavigate, useParams } from "react-router";
 import { jwtDecode } from "jwt-decode";
 import { createAnswerService } from "../../../service/answer.service";
 
 const CreateAnswerComp = () => {
   const [message, setMessage] = useState("");
-  const [isLargeScreen, setIsLargeScreen] = useState(window.innerWidth > 992);
 
   const { question_id } = useParams();
 
@@ -50,59 +47,38 @@ const CreateAnswerComp = () => {
     }
   };
 
-  useEffect(() => {
-    const handleResize = () => {
-      setIsLargeScreen(window.innerWidth > 992);
-    };
-
-    window.addEventListener("resize", handleResize);
-    return () => {
-      window.removeEventListener("resize", handleResize);
-    };
-  }, []);
-
   return (
-    <div>
-      <div className="main-content-area ptb-100">
-        <div className="container">
-          <div className="row">
-            <ContentLeft />
-
-            {/* Main content of the ask question page */}
-            <div className="col-lg">
-              <div className="middull-content">
-                <form className="your-answer-form" onSubmit={handleSubmit}>
-                  <div className="form-group">
-                    <h3>Create Your Answer</h3>
-                  </div>
-                  <p
-                    className={
-                      message.includes("success")
-                        ? "text-success"
-                        : "text-danger"
-                    }
-                  >
-                    {message}
-                  </p>
-                  <div className="form-group">
-                    <input
-                      type="text"
-                      ref={answerDom}
-                      className="form-control"
-                      placeholder="Title"
-                    />
-                  </div>
-
-                  <div className="form-group">
-                    <button type="submit" className="default-btn">
-                      Post Your Answer
-                    </button>
-                  </div>
-                </form>
+    <div className="main-content-area pb-100 mx-auto py-4 md:w-150 bg-gray-200">
+      <div className="container">
+        {/* Main content of the ask question page */}
+        <div>
+          <div className="middull-content">
+            <form className="your-answer-form" onSubmit={handleSubmit}>
+              <div className="form-group">
+                <h3>Create Your Answer</h3>
               </div>
-            </div>
+              <p
+                className={
+                  message.includes("success") ? "text-success" : "text-danger"
+                }
+              >
+                {message}
+              </p>
+              <div className="form-group">
+                <input
+                  type="text"
+                  ref={answerDom}
+                  className="form-control"
+                  placeholder="Title"
+                />
+              </div>
 
-            {isLargeScreen && <ContentRight />}
+              <div className="form-group">
+                <button type="submit" className="default-btn">
+                  Post Your Answer
+                </button>
+              </div>
+            </form>
           </div>
         </div>
       </div>
